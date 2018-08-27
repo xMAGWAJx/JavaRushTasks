2e64d2a7017de7ab70759a7482df31c71f50f929
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 15:53:13 +0300

    task0138 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("");
+        System.out.println("Under the lights where we stand tall");
+        System.out.println("Nobody touches us at all");
+        System.out.println("Showdown, shootout, spread fear within, without");
+        System.out.println("We're gonna take what's ours to have");
+        System.out.println("Spread the word throughout the land");
+        System.out.println("They say the bad guys wear black");
+        System.out.println("We're tagged and can't turn back");
+
+        //done
+    }
+}
