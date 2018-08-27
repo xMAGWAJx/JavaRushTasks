9bdcfa5817baff8d856b4c82ac350b0c594fe4d9
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 16:36:43 +0300

    task0204 - done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
@@ -5,13 +5,10 @@ package com.javarush.task.task02.task0202;
 */
 public class Solution {
     public static void main(String[] args) {
-        Person person = new Person();//напишите тут ваш код
+        //напишите тут ваш код
     }
 
     public static class Person {
-        String name = "Ruslan";
-        int age = 31;
-        int weight = 83;
-        int money = 100;//напишите тут ваш код
+        //напишите тут ваш код
     }
 }
