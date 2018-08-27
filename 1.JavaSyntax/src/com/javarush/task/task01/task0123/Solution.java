21d480fc909e4f2b2507d8b7f36cec529f7ea9d2
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 15:25:55 +0300

    task0123 - done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0123/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0123/Solution.java
@@ -9,11 +9,11 @@ public class Solution {
         int a = 10;
         int b = 15;
         double c = b + 38;
-        int d = a + 12;
-        double e = 12.3;
+      //  int d = a + 12;
+      //  double e = 12.3;
         String s = "s" + a;
         String s1 = a + "b";
-        String s2 = "a";
+      //  String s2 = "a";
         String s3 = s1 + "a";
         String s4 = s3 + "b";
         System.out.println(c + s4 + s);
