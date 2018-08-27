c35e4fb7a7444d68a35efbdcb5175e03a4d5ba24
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 15:05:54 +0300

    task0117 - done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0117/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0117/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         String s1 = "Coding";
         String s2 = "in";
-        String s3 = "pascal";
+        String s3 = "java";
         System.out.println(s1 + " " + s2 + " " + s3);
     }
 }
