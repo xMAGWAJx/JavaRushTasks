1eb276576e122bedbbb1cdaeae08a7f759142529
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 17:14:30 +0300

    task0212 - done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0212/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0212/Solution.java
@@ -4,14 +4,21 @@ package com.javarush.task.task02.task0212;
 Великолепная восьмерка
 */
 public class Solution {
-    public static void main(String[] args) {
-        //напишите тут ваш код
-        Cat cat8 = new Cat();
-        Cat cat9;
-        //напишите тут ваш код
-    }
+  public static void main(String[] args) {
+    Cat cat0;
+    Cat cat1 = new Cat();
+    Cat cat2 = new Cat();
+    Cat cat3 = new Cat();
+    Cat cat4 = new Cat();
+    Cat cat5 = new Cat();
+    Cat cat6 = new Cat();
+    Cat cat7 = new Cat();
+    Cat cat8 = new Cat();
+    Cat cat9;
 
-    public static class Cat {
+  }
 
-    }
+  public static class Cat {
+
+  }
 }
