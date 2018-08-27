86de16ddf67abf0540db7791ee1b8ce6fb6900f5
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 17:03:01 +0300

    task0209 - done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0209/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0209/Solution.java
@@ -4,14 +4,17 @@ package com.javarush.task.task02.task0209;
 Макс, Белла и Джек
 */
 public class Solution {
-    public static void main(String[] args) {
-        Dog dog1 = new Dog();
-        dog1.name = "Max";
+  public static void main(String[] args) {
+    Dog dog1 = new Dog();
+    dog1.name = "Max";
+    Dog dog2 = new Dog();
+    dog2.name = "Bella";
+    Dog dog3 = new Dog();
+    dog3.name = "Jack";
+    //напишите тут ваш код
+  }
 
-        //напишите тут ваш код
-    }
-
-    public static class Dog {
-        public String name;
-    }
+  public static class Dog {
+    public String name;
+  }
 }
