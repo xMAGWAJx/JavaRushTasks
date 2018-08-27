b042a3cff8443108a41c26dbcabb29dea647fbfd
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 15:34:46 +0300

    task0125 - done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0125/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0125/Solution.java
@@ -5,14 +5,14 @@ package com.javarush.task.task01.task0125;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        String s = "15";
-        a = 5;
-        int z = 18;
-        int d = 18;
-        c = a + d;
-        b = " better than ";
+  public static void main(String[] args) {
+    // String s = "15";
+    int a = 5;
+    //int z = 18;
+    int d = 18;
+    int c = a + d;
+    String b = " better than ";
 
-        System.out.println(a + b + c);
-    }
+    System.out.println(a + b + c);
+  }
 }
