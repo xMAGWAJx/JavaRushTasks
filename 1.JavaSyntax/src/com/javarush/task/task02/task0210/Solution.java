df7562a83bb4ee7bf65bfbf493f5fd44038d0c25
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 17:04:02 +0300

    task0210 - done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0210/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0210/Solution.java
@@ -5,6 +5,6 @@ package com.javarush.task.task02.task0210;
 */
 public class Solution {
     public static void main(String[] args) {
-      System.out.println("Будешь плохо кодить, придет Java и съест твою память");//напишите тут ваш код
+      System.out.println("Будешь плохо кодить, придет Java и съест твою память");//done
     }
 }
