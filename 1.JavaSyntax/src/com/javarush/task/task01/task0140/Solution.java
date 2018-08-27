7906de99edaaab0800de3bf7dd10fc109329cc5e
xMAGWAJx <xmagwajx@gmail.com>
2018-08-27 14:59:15 +0300

    task0140 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0140;
+
+/* 
+Выводим квадрат числа
+*/
+
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        int kvadrat = number * number;
+        System.out.println(kvadrat);
+
+        //напишите тут ваш код
+    }
+}
