201812d131552c71ca0961771f7d4f2af8827b18
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:39:26 +0300

    task0320 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0320/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0320/Solution.java
@@ -6,9 +6,12 @@ package com.javarush.task.task03.task0320;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+      System.out.println(name + " зарабатывает $5,000. Ха-ха-ха!");
     }
 }
