550b0f7d67e4d3c9b670e46889997cad45fd3275
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:32:57 +0300

    task0318 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
@@ -5,9 +5,13 @@ package com.javarush.task.task03.task0318;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      String name = scanner.nextLine();
+      int age = scanner.nextInt();
+      System.out.println(name + " захватит мир через " + age + " лет. Му-ха-ха!");
     }
 }
