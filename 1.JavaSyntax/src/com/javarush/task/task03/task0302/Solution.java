ee4a0ffd90d7dc78a20cfdefeee6e1bdd9526066
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 13:08:50 +0300

    task0302 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0302/Solution.java
@@ -1,10 +1,13 @@
 package com.javarush.task.task03.task0302;
 
-/* 
+/*
 Немедленно в печать
 */
 public class Solution {
-    //напишите тут ваш код
+  public static void printString(String s)
+  {
+        System.out.println(s);
+  }
 
     public static void main(String[] args) {
         printString("Hello Amigo!");
