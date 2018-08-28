1c1e30609396bf2cd55aedff6a2708d6d34717ca
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:27:57 +0300

    task0317 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0317/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0317/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task03.task0317;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      System.out.println("日本語");
     }
 }
