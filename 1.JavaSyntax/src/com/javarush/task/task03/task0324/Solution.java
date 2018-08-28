654d2afd6d645eb8b206bbf751c66f53396b4631
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 17:01:28 +0300

    task0324 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
@@ -6,6 +6,8 @@ package com.javarush.task.task03.task0324;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      for (int i=1; i<=10; i++){
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+      }
     }
 }
