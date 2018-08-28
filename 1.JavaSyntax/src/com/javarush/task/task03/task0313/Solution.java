6c50bae79b08d383da114ac10d387e4c67cd5075
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:12:10 +0300

    task0313 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
@@ -6,6 +6,17 @@ package com.javarush.task.task03.task0313;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      String arra[]=new String[3];
+      arra[0]="Мама";
+      arra[1]="Мыла";
+      arra[2]="Раму";
+
+      for (int a=0;a<3;a++) {
+        for (int b = 0; b < 3; b++) {
+          for (int c = 0; c < 3; c++)
+            if (c != b && b!=a && c!=a)
+              System.out.println(arra[a]+arra[b]+arra[c]);
+        }
+      }
     }
 }
