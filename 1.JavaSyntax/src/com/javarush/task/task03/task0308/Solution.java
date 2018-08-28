02ea9c062ce57da604a46752ff86e4c7b340381b
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 13:39:30 +0300

    task0308 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task03.task0308;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      System.out.println(1*2*3*4*5*6*7*8*9*10);
     }
 }
