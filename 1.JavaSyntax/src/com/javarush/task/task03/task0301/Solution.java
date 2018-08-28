6e518aaa50b4b53d98da0aab9cef225c27280b6f
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 12:59:36 +0300

    task0301 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0301/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0301/Solution.java
@@ -12,6 +12,7 @@ public class Solution {
     }
 
     public static void div(int a, int b) {
-        //напишите тут ваш код
+        int c = a / b;
+      System.out.println(c);//напишите тут ваш код
     }
 }
