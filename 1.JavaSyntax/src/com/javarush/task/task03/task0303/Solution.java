3a47582b152e40168a66ff931d11e55857398004
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 13:12:56 +0300

    task0303 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -6,10 +6,12 @@ package com.javarush.task.task03.task0303;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      System.out.println(convertEurToUsd(5, 5));
+      System.out.println(convertEurToUsd(5, 7));
     }
 
     public static double convertEurToUsd(int eur, double course) {
-        //напишите тут ваш код
+      double Usd= eur*course;
+        return Usd;
     }
 }
