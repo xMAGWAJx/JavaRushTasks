e82f2ba907195a5ead433f67edbaaa048b3f56ba
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:42:27 +0300

    task0322 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0322/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0322/Solution.java
@@ -6,9 +6,14 @@ package com.javarush.task.task03.task0322;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.nextLine();
+        String name2 = scanner.nextLine();
+        String name3 = scanner.nextLine();
+      System.out.println(name1 + " + " + name2 + " + " + name3 + " = Чистая любовь, да-да!");
     }
 }
