3b9b7895a960e9761626ff99a408297266984f36
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:25:58 +0300

    task0316 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
@@ -6,6 +6,9 @@ package com.javarush.task.task03.task0316;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      String s = "\\"+"\\";
+      String p = "\\"+"\"";
+      System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
+      System.out.println("It's Java string: "+p+"C:"+s+"Program Files"+s+"Java"+s+"jdk1.7.0"+s+"bin"+p );
     }
 }
