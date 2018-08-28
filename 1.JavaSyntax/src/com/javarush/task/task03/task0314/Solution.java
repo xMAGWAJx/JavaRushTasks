afbd0fb012ac9720f8c7185ca6e641c9d39fcb32
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:18:40 +0300

    task0314 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -6,6 +6,13 @@ package com.javarush.task.task03.task0314;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      int t,k;
+      int table[][] = new int[10][10];
+      for (t = 1; t < 11; t++){
+        for (k = 1; k < 11; k++){
+          System.out.print(t * k + " ");
+        }
+        System.out.println();
+      }
     }
 }
