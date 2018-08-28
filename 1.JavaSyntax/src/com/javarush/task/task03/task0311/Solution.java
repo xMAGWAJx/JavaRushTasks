314f5b64cb472987cf5f595a72e3d026c8a16261
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 15:55:37 +0300

    task0311 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0311/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0311/Solution.java
@@ -10,6 +10,6 @@ public class Solution {
     }
 
     public static void writeToConsole(String s) {
-        //напишите тут ваш код
+      System.out.println("printing: " + s);//напишите тут ваш код
     }
 }
