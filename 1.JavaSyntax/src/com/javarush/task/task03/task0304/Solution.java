66c9e78730fbe47470e6ff0b73bdf804aa590f9f
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 13:16:22 +0300

    task0304 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0304/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0304/Solution.java
@@ -6,7 +6,8 @@ package com.javarush.task.task03.task0304;
 
 public class Solution {
     public static double addTenPercent(int i) {
-        //напишите тут ваш код
+        double a = i * 1.1;
+      return a;//напишите тут ваш код
     }
 
     public static void main(String[] args) {
