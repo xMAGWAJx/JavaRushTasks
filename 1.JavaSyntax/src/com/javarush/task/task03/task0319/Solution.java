84242cae15a3ccd3cd9c3e3b99bc57dd984804e5
xMAGWAJx <xmagwajx@gmail.com>
2018-08-28 16:37:34 +0300

    task0319 - done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -5,9 +5,15 @@ package com.javarush.task.task03.task0319;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+      System.out.println(name + " получает " + number1 + " через " + number2 + " лет.");
+
     }
 }
