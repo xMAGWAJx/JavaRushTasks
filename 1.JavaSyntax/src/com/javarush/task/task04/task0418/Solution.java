3eac9870b285c54aa72b51e1d32d4d4bb2f55209
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 14:40:32 +0300

    task0418 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0418/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0418/Solution.java
@@ -5,9 +5,14 @@ package com.javarush.task.task04.task0418;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+      System.out.println(Math.min(a, b));
+
     }
 }
