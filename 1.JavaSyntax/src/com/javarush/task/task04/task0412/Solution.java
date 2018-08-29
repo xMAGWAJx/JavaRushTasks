3e7814350a96eb59d2df4f841a2f0f6ca5b3b5c8
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 13:59:49 +0300

    task0412 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
@@ -5,10 +5,20 @@ package com.javarush.task.task04.task0412;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number > 0) {
+          System.out.println(number * 2);
+        }
+        else if (number < 0) {
+          System.out.println(number + 1);
+        }
+        else
+        System.out.println(number);
 
     }
 
