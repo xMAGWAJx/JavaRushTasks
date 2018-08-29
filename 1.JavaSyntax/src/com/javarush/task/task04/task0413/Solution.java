b5291f2e1c7a6319041c3011a2092fd07cb5cc69
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 14:04:08 +0300

    task0413 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0413/Solution.java
@@ -5,9 +5,35 @@ package com.javarush.task.task04.task0413;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+  public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+    int day = scanner.nextInt();
+    if (day == 1) {
+      System.out.println("понедельник");
     }
+    if (day == 2) {
+      System.out.println("вторник");
+    }
+    if (day == 3) {
+      System.out.println("среда");
+    }
+    if (day == 4) {
+      System.out.println("четвер");
+    }
+    if (day == 5) {
+      System.out.println("пятница");
+    }
+    if (day == 6) {
+      System.out.println("суббота");
+    }
+    if (day == 7) {
+      System.out.println("воскресенье");
+    }
+    if (day > 7) {
+      System.out.println("такого дня недели не существует");
+    }
+  }
 }
