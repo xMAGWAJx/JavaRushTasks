eea6853ed0e82d0e61d75ad60e60374a3835b052
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 11:46:19 +0300

    task0410 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0410/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task04.task0410;
+
+/* 
+Попадём-не-попадём
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        checkInterval(60);
+        checkInterval(112);
+        checkInterval(10);
+    }
+
+    public static void checkInterval(int a) {
+        //напишите тут ваш код
+    }
+}
