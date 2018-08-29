e7bb70a046314cc89b9225373364d057fe0907b7
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 16:03:19 +0300

    task0420 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
@@ -5,9 +5,22 @@ package com.javarush.task.task04.task0421;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      String name1 = scanner.nextLine();
+      String name2 = scanner.nextLine();
+
+      if (name1.equals(name2)) {
+        System.out.println("Имена идентичны");
+      }
+      else if (name1.length() == name2.length()) {
+        System.out.println("Длины имен равны");
+      }
+      /*if ((name1 != name2) && (name1.length() != name2.length())){
+        System.out.println();
+      }*/
     }
 }
