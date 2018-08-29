3be318c1494a75cd6fb633258e122ddb595def2d
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 14:14:47 +0300

    task0414 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -8,6 +8,13 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
+      String name = reader.readLine();
+      int y = Integer.parseInt(name);
+      if (y%4==0 && y%100 !=0 || y%400==0) {
+        System.out.println("количество дней в году: 366.");}
+      else{
+        System.out.println("количество дней в году: 365.");
+      }
     }
 }
