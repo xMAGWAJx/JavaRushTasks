48d025893da031b44f1d2f7b8294b0a142ccda30
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 16:41:23 +0300

    task0424 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
@@ -5,9 +5,23 @@ package com.javarush.task.task04.task0424;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+      int c = scanner.nextInt();
+
+      if (a == b) {
+        System.out.println("3");
+      }
+      else if (a == c) {
+        System.out.println("2");
+      }
+      else if (c == b) {
+        System.out.println("1");
+      }
     }
 }
