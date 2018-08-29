d1cec6168d5d046f6d224bf98e1e5cd6437293d1
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 11:35:28 +0300

    task0408 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0408/Solution.java
@@ -12,6 +12,14 @@ public class Solution {
     }
 
     public static void compare(int a) {
-        //напишите тут ваш код
+        if (a < 5) {
+          System.out.println("Число меньше 5");
+        }
+      if (a > 5) {
+        System.out.println("Число больше 5");
+      }
+      if (a == 5){
+        System.out.println("Число равно 5");
+      }
     }
 }
