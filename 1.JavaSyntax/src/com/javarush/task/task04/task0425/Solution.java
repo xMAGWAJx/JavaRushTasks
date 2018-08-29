029e8e38157a376095ce5dfed83a7404231bdcf0
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 16:53:53 +0300

    task0425 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
@@ -5,9 +5,25 @@ package com.javarush.task.task04.task0425;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a > 0 && b > 0) {
+          System.out.println(1);
+        }
+        else if (a < 0 && b > 0) {
+          System.out.println(2);
+        }
+        else if (a < 0 && b < 0) {
+          System.out.println(3);
+        }
+        else if (a > 0 && b < 0) {
+          System.out.println(4);
+        }
     }
 }
