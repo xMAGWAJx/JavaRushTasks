3338299ce36b141fef083780fee5db93458b09c3
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 15:44:48 +0300

    task0420 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -15,7 +15,22 @@ public class Solution {
         b = s.nextInt();
         c = s.nextInt();
 
-        System.out.println();
-
+      if (a >= b && b >= c)
+        System.out.println(a + " " + b + " " + c);
+      else
+      if (a >= c && c >= b)
+        System.out.println(a + " " + c + " " + b);
+      else
+      if (b >= a && a >= c)
+        System.out.println(b + " " + a + " " + c);
+      else
+      if (b >= c && c >= a)
+        System.out.println(b + " " + c + " " + a);
+      else
+      if (c >= a && a >= b)
+        System.out.println(c + " " + a + " " + b);
+      else
+      if (c >= b && b >= a)
+        System.out.println(c + " " + b + " " + a);
     }
 }
