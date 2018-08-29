c238699a78e53cf2e7a90cfc761111aca534f341
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 14:54:22 +0300

    task0420 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task04.task0420;
+
+/* 
+Сортировка трех чисел
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int a, b, c;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+
+        System.out.println();
+
+    }
+}
