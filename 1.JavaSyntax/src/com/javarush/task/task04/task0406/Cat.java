a718840083a65dd1afeb2ea094a4ce8cada821f5
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 11:23:42 +0300

    task0406 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0406/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0406/Cat.java
@@ -10,7 +10,7 @@ public class Cat {
     public void setName(String firstName, String lastName) {
         String fullName = firstName + " " + lastName;
 
-        //напишите тут ваш код
+        this.fullName = fullName;
     }
 
     public static void main(String[] args) {
