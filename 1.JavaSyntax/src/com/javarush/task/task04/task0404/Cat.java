5b282503aed226707e6f8634cd7264791fdb7974
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 10:23:30 +0300

    task0404 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0404/Cat.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task04.task0404;
+
+/* 
+Учет котов
+*/
+
+public class Cat {
+    private static int catsCount = 0;
+
+    public static void addNewCat() {
+        //напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
