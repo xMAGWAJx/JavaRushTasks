32b19022040103b2d25d09c367b5d0f8798e5726
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 16:15:47 +0300

    task0422 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0422/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0422/Solution.java
@@ -5,9 +5,16 @@ package com.javarush.task.task04.task0422;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+
+        if (age < 18) {
+          System.out.println("Подрасти еще");
+        }
     }
 }
