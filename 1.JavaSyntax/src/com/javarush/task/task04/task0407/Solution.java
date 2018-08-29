3c9f81e10567ccdf6c1d3687b01960a896f1af51
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 11:27:46 +0300

    task0407 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0407/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0407/Solution.java
@@ -7,10 +7,10 @@ package com.javarush.task.task04.task0407;
 public class Solution {
     public static void main(String[] args) {
         Cat cat1 = new Cat();
-        //напишите тут ваш код
+        cat1.count++;
 
         Cat cat2 = new Cat();
-        //напишите тут ваш код
+        cat2.count++;
 
         System.out.println("Cats count is " + Cat.count);
     }
