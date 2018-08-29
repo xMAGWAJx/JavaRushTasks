637406d54fbb7c2545ce5a7b3ea6389c77d6402f
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 14:19:58 +0300

    task0415 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -8,6 +8,15 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      int a = Integer.parseInt( reader.readLine() );
+      int b = Integer.parseInt( reader.readLine() );
+      int c = Integer.parseInt( reader.readLine() );
+      if ( a + b > c && b + c > a && a + c > b ){
+        System.out.println("Треугольник существует.");
+      }
+      else{
+        System.out.println("Треугольник не существует.");
+      }
     }
 }
