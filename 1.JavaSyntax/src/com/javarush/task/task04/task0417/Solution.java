0827513146e866afc9041d908c7f56d847ccde44
xMAGWAJx <xmagwajx@gmail.com>
2018-08-29 14:28:33 +0300

    task0417 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
@@ -5,9 +5,19 @@ package com.javarush.task.task04.task0417;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+      int c = scanner.nextInt();
+      if (a == b || a == c)
+        System.out.print(a);
+      if (b == a || b == c)
+        System.out.print(" " + b);
+      if (c == a || c == b)
+        System.out.print(" " + c);
     }
 }
