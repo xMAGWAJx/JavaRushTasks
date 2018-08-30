5e536758993591cccd8f1001b7435abcf042ea7b
xMAGWAJx <xmagwajx@gmail.com>
2018-08-30 15:31:28 +0300

    task0435 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
@@ -6,7 +6,10 @@ package com.javarush.task.task04.task0435;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        for (int i = 1; i <= 100; i++) {
+          if (i % 2 == 0)
+          System.out.println(i);
+        }
 
     }
 }
