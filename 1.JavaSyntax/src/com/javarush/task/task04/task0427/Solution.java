059d8c5a3bb72d6077fcb82f47cc9f5ada40f85f
xMAGWAJx <xmagwajx@gmail.com>
2018-08-30 10:33:25 +0300

    task0427 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
@@ -5,10 +5,30 @@ package com.javarush.task.task04.task0427;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int number = scanner.nextInt();
 
+      if ((number % 2) == 0 && (number > 0 && number <= 9)) {
+        System.out.println("четное однозначное число");
+      }
+      if ((number%2) !=0 && (number > 0 && number <= 9)) {
+        System.out.println("нечетное однозначное число");
+      }
+      if ((number % 2) == 0 && (number >= 10 && number <= 99)) {
+        System.out.println("четное двузначное число");
+      }
+      if ((number%2) !=0 && (number >= 10 && number <= 99)) {
+        System.out.println("нечетное двузначное число");
+      }
+      if ((number % 2) == 0 && (number >= 100 && number <= 999)) {
+        System.out.println("четное трехзначное число");
+      }
+      if ((number%2) !=0 && (number >= 100 && number <= 999)) {
+        System.out.println("нечетное трехзначное число");
+      }
     }
 }
