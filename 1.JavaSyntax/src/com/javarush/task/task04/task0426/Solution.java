d6f94dc6ea110d6a7353d734a76330cfee9e6882
xMAGWAJx <xmagwajx@gmail.com>
2018-08-30 10:00:28 +0300

    task0426 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -5,9 +5,27 @@ package com.javarush.task.task04.task0426;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (number == 0) {
+          System.out.println("ноль");
+        }
+        if ((number % 2) == 0 && number > 0) {
+          System.out.println("положительное четное число");
+        }
+        if ((number%2) !=0 && number > 0) {
+          System.out.println("положительное нечетное число");
+        }
+        if ((number % 2) == 0 && number < 0) {
+        System.out.println("отрицательное четное число");
+        }
+        if ((number%2) !=0 && number < 0) {
+        System.out.println("отрицательное нечетное число");
+      }
     }
 }
