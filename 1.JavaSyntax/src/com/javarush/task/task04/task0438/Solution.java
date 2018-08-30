7512ca803ab40dccb0b684027abfb0b051843abc
xMAGWAJx <xmagwajx@gmail.com>
2018-08-30 16:45:06 +0300

    task0438 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -5,8 +5,13 @@ package com.javarush.task.task04.task0438;
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+  public static void main(String[] args) throws Exception {
+    for (int a = 0; a < 10; a++) {
+      System.out.print(8);
+    }
+    for (int a = 0; a < 10; a++) {
+      System.out.println(8);
 
     }
+  }
 }
