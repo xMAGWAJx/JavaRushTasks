91b825e400589e4742bb028077a6f09ce08e5119
xMAGWAJx <xmagwajx@gmail.com>
2018-08-30 12:16:09 +0300

    task0429 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0429/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0429/Solution.java
@@ -5,10 +5,28 @@ package com.javarush.task.task04.task0429;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+      int c = scanner.nextInt();
+
+      int countPositive = 0;
+      int countNegative = 0;
+
+      if (a > 0) countPositive++;
+      if (b > 0) countPositive++;
+      if (c > 0) countPositive++;
+
+      if (a < 0) countNegative++;
+      if (b < 0) countNegative++;
+      if (c < 0) countNegative++;
+
+      System.out.println("количество отрицательных чисел: " + countNegative);
+      System.out.println("количество положительных чисел: " + countPositive);
 
     }
 }
