fa66bc779cfbce225b19059bbfe511f56258cffe
xMAGWAJx <xmagwajx@gmail.com>
2018-08-30 16:06:23 +0300

    task0436 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0436/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0436/Solution.java
@@ -6,10 +6,20 @@ package com.javarush.task.task04.task0436;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int m = scanner.nextInt();
+      int n = scanner.nextInt();
 
+      for (int a = 1; a <= m; a++) {
+        System.out.print(8);
+        for (int b = 1; b <n; b++){
+          System.out.print(8);
+        }
+        System.out.println();
+      }
     }
 }
