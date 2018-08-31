1419f883534248282c6f99884592b8dbd14646c4
xMAGWAJx <xmagwajx@gmail.com>
2018-08-31 16:43:40 +0300

    task0508 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0508/Person.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0508/Person.java
@@ -5,9 +5,35 @@ package com.javarush.task.task05.task0508;
 */
 
 public class Person {
-    //напишите тут ваш код
+    String name = "Pepa";
+    int age = 100500;
+    char sex = 'm';
 
-    public static void main(String[] args) {
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public char getSex() {
+    return sex;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setSex(char sex) {
+    this.sex = sex;
+  }
+
+  public static void main(String[] args) {
 
     }
 }
