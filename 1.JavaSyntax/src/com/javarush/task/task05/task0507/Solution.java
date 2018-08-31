89966f135f6482e4a61d0b11397a2c590c2e2e5a
xMAGWAJx <xmagwajx@gmail.com>
2018-08-31 16:41:26 +0300

    task0507 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -4,9 +4,29 @@ package com.javarush.task.task05.task0507;
 Среднее арифметическое
 */
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+          double num = 0;
+          double sum = 0;
+          int count = 0;
+
+          Scanner scanner = new Scanner(System.in);
+          while (num != -1) {
+            int scanned = scanner.nextInt();
+            num = Double.parseDouble(String.valueOf(scanned));
+            if (num != -1) {
+              sum += num;
+              count++;
+            }
+          }
+          if (count != 0) {
+            System.out.println(sum / count);
+          }
+          else {
+            System.out.println("Nothing");
+          }
     }
 }
 
