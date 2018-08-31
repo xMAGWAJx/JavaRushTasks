dbaa821ced00c6b92376ecb9a07ca38d23fccdc1
xMAGWAJx <xmagwajx@gmail.com>
2018-08-31 15:07:48 +0300

    task0501 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -5,7 +5,11 @@ package com.javarush.task.task05.task0501;
 */
 
 public class Cat {
-    //напишите тут ваш код
+
+    String name = "Vaska";
+    int age = 150;
+    int weight = 500;
+    int strength = 9000;
 
     public static void main(String[] args) {
 
