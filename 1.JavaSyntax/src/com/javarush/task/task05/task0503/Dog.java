d137ade12fa8e1c1f3d13fd99781ed689c0e740c
xMAGWAJx <xmagwajx@gmail.com>
2018-08-31 15:56:52 +0300

    task0503 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
@@ -6,7 +6,24 @@ package com.javarush.task.task05.task0503;
 */
 
 public class Dog {
-    //напишите тут ваш код
+    String name = "Biba";
+    int age = 100500;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
 
     public static void main(String[] args) {
 
