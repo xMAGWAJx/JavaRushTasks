61c132ec2fe5f9c80959a56847344e313a1708d9
xMAGWAJx <xmagwajx@gmail.com>
2018-08-31 14:24:44 +0300

    task0443 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
@@ -6,9 +6,17 @@ package com.javarush.task.task04.task0443;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      String name = scanner.nextLine();
+      int y = scanner.nextInt();
+      int m = scanner.nextInt();
+      int d = scanner.nextInt();
+
+      System.out.println("Меня зовут " + name + ".");
+      System.out.print("Я родился " + d + "." + m + "." + y);
     }
 }
