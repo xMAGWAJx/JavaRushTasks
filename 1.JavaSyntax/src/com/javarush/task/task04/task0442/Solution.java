e4da6b3cba8131905cee97180cb80b041aa74176
xMAGWAJx <xmagwajx@gmail.com>
2018-08-31 14:20:14 +0300

    task0442 - done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -6,9 +6,19 @@ package com.javarush.task.task04.task0442;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+  public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+
+    int sum = 0;
+    for (int b = 0; b != -1; b++) {
+      int a = scanner.nextInt();
+      sum += a;
+      if (a == -1)
+        break;
     }
+    System.out.println(sum);
+  }
 }
