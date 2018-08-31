dea3efb09ceea00d627d82dfd2cb771085bb5caa
xMAGWAJx <xmagwajx@gmail.com>
2018-08-31 15:51:16 +0300

    task0502 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -13,7 +13,27 @@ public class Cat {
     }
 
     public boolean fight(Cat anotherCat) {
-        //напишите тут ваш код
+        Cat cat1 = new Cat();
+        cat1.age = 6;
+        cat1.weight = 6;
+        cat1.strength = 6;
+
+        Cat cat2 = new Cat();
+        cat2.age = 5;
+        cat2.weight = 4;
+        cat2.strength = 7;
+
+      if (age > anotherCat.age) {
+        return true;
+      }
+      else if (weight > anotherCat.weight) {
+        return true;
+      }
+      else if (strength > anotherCat.strength) {
+        return true;
+      }
+      return false;
+
     }
 
     public static void main(String[] args) {
