ce3356ac978bc1b7a9c1eb850f3eea2678816bf4
xMAGWAJx <xmagwajx@gmail.com>
2018-09-03 15:40:05 +0300

    task0517 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0517/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0517/Cat.java
@@ -5,7 +5,41 @@ package com.javarush.task.task05.task0517;
 */
 
 public class Cat {
-    //напишите тут ваш код
+    String name;
+    int age = 99;
+    int weight = 100500;
+    String address;
+    String color = "Black";
+
+    public Cat (String name) {
+      this.name = name;
+      this.age = age;
+      this.weight = weight;
+      this.color = color;
+    }
+    public Cat (String name,int weight, int age) {
+      this.name = name;
+      this.age = age;
+      this.weight = weight;
+      this.color = color;
+    }
+    public Cat (String name, int age) {
+      this.name = name;
+      this.age = age;
+      this.weight = weight;
+      this.color = color;
+    }
+    public Cat (int weight, String color) {
+      this.age = age;
+      this.weight = weight;
+      this.color = color;
+    }
+    public Cat (int weight, String color, String address) {
+      this.age = age;
+      this.weight = weight;
+      this.address = address;
+      this.color = color;
+    }
 
     public static void main(String[] args) {
 
