9814b5f2d4b57362f10f495fa365f0afdc7453a6
xMAGWAJx <xmagwajx@gmail.com>
2018-09-03 14:59:42 +0300

    task0513 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
@@ -5,7 +5,32 @@ package com.javarush.task.task05.task0513;
 */
 
 public class Rectangle {
-    //напишите тут ваш код
+    int top;
+    int left;
+    int width;
+    int height;
+
+    public void initialize (int top) {
+      this.top = top;
+    }
+
+    public void initialize (int top, int left) {
+      this.top = top;
+      this.left = left;
+    }
+
+    public void initialize (int top, int left, int witdth) {
+      this.top = top;
+      this.left = left;
+      this.width = witdth;
+    }
+
+    public void initialize (int top, int left, int width, int height) {
+      this.top = top;
+      this.left = left;
+      this.width = width;
+      this.height = height;
+    }
 
     public static void main(String[] args) {
 
