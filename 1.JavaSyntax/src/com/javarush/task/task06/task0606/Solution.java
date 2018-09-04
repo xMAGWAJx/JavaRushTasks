afb6ef11b540b63a41ea60dc7069b71f4ec5b07f
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 16:37:38 +0300

    task0605 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task06.task0606;
 
 import java.io.*;
+import java.util.Scanner;
 
 /* 
 Чётные и нечётные циферки
@@ -12,6 +13,14 @@ public class Solution {
     public static int odd;
 
     public static void main(String[] args) throws IOException {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int number = scanner.nextInt();
+
+      while (number != 0) {
+        if (number % 2 == 0) even++;
+        else odd++;
+        number /= 10;
+      }
+      System.out.println("Even: " + even + " Odd: " + odd);
     }
 }
