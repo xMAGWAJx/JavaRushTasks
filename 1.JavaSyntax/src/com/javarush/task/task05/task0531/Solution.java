a781dcfe52a5c05bd8ab0186d74a92ba0c14245d
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 15:10:05 +0300

    task0531 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
@@ -13,14 +13,19 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
 
-        int minimum = min(a, b);
+        int minimum = min(a, b, c, d, e);
 
         System.out.println("Minimum = " + minimum);
     }
+    public static int min (int a, int b) {
+      return a < b ? a : b;
+    }
 
-
-    public static int min(int a, int b) {
-        return a < b ? a : b;
+    public static int min(int a, int b, int c, int d, int e) {
+      return min(min(min(a, b), min(c, d)),e);
     }
 }
