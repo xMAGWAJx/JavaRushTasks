264c1b53810a1d0a05b572bcd91c17544e8573d9
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 13:37:00 +0300

    task0528 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
@@ -4,8 +4,13 @@ package com.javarush.task.task05.task0528;
 Вывести на экран сегодняшнюю дату
 */
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+      Date dateNow  = new Date();
+      SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd MM yyyy");
+      System.out.println(formatForDateNow.format(dateNow ));
     }
 }
