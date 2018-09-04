f1f85a0a0b0b9744b1ea92eadaf2ea4bd7a312e6
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 17:18:19 +0300

    task0608 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0608/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0608/Cat.java
@@ -12,12 +12,12 @@ public class Cat {
     }
 
     public static int getCatCount() {
-        //напишите тут ваш код
+      return catCount;
 
     }
 
     public static void setCatCount(int catCount) {
-        //напишите тут ваш код
+        Cat.catCount = catCount;
 
     }
 
