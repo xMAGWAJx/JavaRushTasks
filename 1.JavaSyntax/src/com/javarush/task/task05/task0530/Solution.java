f444bf8f6136389421984201f19b363b646b1827
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 14:23:29 +0300

    task0530 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0530/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0530/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task05.task0530;
 
 import java.io.*;
+import java.util.Scanner;
 
 /* 
 Шеф, что-то не пашет
@@ -8,9 +9,9 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = reader.readLine();
-        int b = reader.read();
+      Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
         int sum = a + b;
         System.out.println("Sum = " + sum);
