b66b85c8aced9786f2ad528a893c512f5bf8e766
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 15:51:10 +0300

    task0602 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0602/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0602/Cat.java
@@ -9,10 +9,14 @@ public class Cat {
 
     }
 
-    //напишите тут ваш код
-
+  @Override
+  protected void finalize() throws Throwable {
+    System.out.println("Cat was destroyed");
+  }
 }
 
 class Dog {
-    //напишите тут ваш код
+    protected void finalize() throws Throwable {
+      System.out.println("Dog was destroyed");
+    }
 }
