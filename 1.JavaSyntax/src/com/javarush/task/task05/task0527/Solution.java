4f7b272fa7542fe2ac6294c0bba2ff1f522779a2
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 13:12:17 +0300

    task0527 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
@@ -5,23 +5,46 @@ package com.javarush.task.task05.task0527;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+  public static void main(String[] args) {
+    Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+    Dog sobakaPepa = new Dog("Pepa", 100500, 300);
+    Cat kotBehemoth = new Cat("Behemoth", 100, 200);
+  }
 
-        //напишите тут ваш код
+  public static class Mouse {
+    String name;
+    int height;
+    int tail;
+
+    public Mouse(String name, int height, int tail) {
+      this.name = name;
+      this.height = height;
+      this.tail = tail;
+    }
+  }
+
+  public static class Dog {
+    String name;
+    int height;
+    int tail;
+
+    public Dog(String name, int height, int tail) {
+      this.name = name;
+      this.height = height;
+      this.tail = tail;
     }
 
-    public static class Mouse {
-        String name;
-        int height;
-        int tail;
+  }
 
-        public Mouse(String name, int height, int tail) {
-            this.name = name;
-            this.height = height;
-            this.tail = tail;
-        }
+  public static class Cat {
+    String name;
+    int height;
+    int tail;
+
+    public Cat(String name, int height, int tail) {
+      this.name = name;
+      this.height = height;
+      this.tail = tail;
     }
-
-    //напишите тут ваш код
+  }
 }
