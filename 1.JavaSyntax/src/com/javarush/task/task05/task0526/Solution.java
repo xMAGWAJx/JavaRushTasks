80987f2668851f7a716ca74a5ae0fa4c7a9892ab
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 11:15:58 +0300

    task0526 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -4,10 +4,40 @@ package com.javarush.task.task05.task0526;
 Мужчина и женщина
 */
 
-public class Solution {
-    public static void main(String[] args) {
-        //напишите тут ваш код
-    }
 
-    //напишите тут ваш код
+public class Solution {
+  public static void main(String[] args) {
+    Man man1 = new Man("Goga", 100, "Hz");
+    Man man2 = new Man("Givi", 99, "Uzdvins");
+    System.out.println(man1.name + " " + man1.age + " " + man1.address);
+    System.out.println(man2.name + " " + man2.age + " " + man2.address);
+    Woman woman1 = new Woman("Pepa", 50, "Kiev");
+    Woman woman2 = new Woman("Uka", 18, "Doka");
+    System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+    System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
+  }
+
+  public static class Man extends Solution {
+    String name;
+    int age;
+    String address;
+
+    public Man (String name, int age, String address) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+  }
+
+  public static class Woman extends Solution{
+    String name;
+    int age;
+    String address;
+
+    public Woman(String name, int age, String address) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+  }
 }
