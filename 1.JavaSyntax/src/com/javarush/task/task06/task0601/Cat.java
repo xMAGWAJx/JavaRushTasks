a80896f086d9de5e601c2279a58b90fcb96cf707
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 15:48:34 +0300

    task0601 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
@@ -5,8 +5,17 @@ package com.javarush.task.task06.task0601;
 */
 
 public class Cat {
-    //напишите тут ваш код
-    public static void main(String[] args) {
+
+  String name;
+  Cat(String name) {
+    this.name = name;
+  }
+
+  protected void finalize() throws Throwable {
+
+  }
+
+  public static void main(String[] args) {
 
     }
 }
