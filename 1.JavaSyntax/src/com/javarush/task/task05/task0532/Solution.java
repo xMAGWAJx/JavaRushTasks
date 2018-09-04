40b75206cff1918f330f5637d74eff63fb310cfb
xMAGWAJx <xmagwajx@gmail.com>
2018-09-04 15:28:08 +0300

    task0532 - done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task05.task0532;
 
 import java.io.*;
+import java.util.Scanner;
 
 /* 
 Задача по алгоритмам
@@ -8,11 +9,22 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = ;
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int maximum = 0;
+        int N = Integer.parseInt(reader.readLine());
 
-        //напишите тут ваш код
-
-        System.out.println(maximum);
+        for (int i = 0; i < N; i++) {
+          int a = Integer.parseInt(reader.readLine());
+          if (i == 0) {
+            maximum = a;
+          }
+          if (a < 0 && a < maximum) {
+            a = maximum;
+          }
+          else if (a > maximum) {
+            maximum = a;
+          }
+        }
+      System.out.println(maximum);
     }
 }
