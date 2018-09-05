ce8db02d3db27c9f98ce3aa21ecb52745fe2cd8e
xMAGWAJx <xmagwajx@gmail.com>
2018-09-05 14:36:49 +0300

    task0609 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0609/Util.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0609/Util.java
@@ -6,7 +6,7 @@ package com.javarush.task.task06.task0609;
 
 public class Util {
     public static double getDistance(int x1, int y1, int x2, int y2) {
-        //напишите тут ваш код
+      return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
     }
 
     public static void main(String[] args) {
