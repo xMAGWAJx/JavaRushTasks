4fbcebd8ae3dbde8cc09bd9b1b13d770db8808d6
xMAGWAJx <xmagwajx@gmail.com>
2018-09-05 15:03:03 +0300

    task0610 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -2,6 +2,7 @@ package com.javarush.task.task06.task0610;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Класс ConsoleReader
@@ -9,23 +10,32 @@ import java.io.InputStreamReader;
 
 public class ConsoleReader {
     public static String readString() throws Exception {
-        //напишите тут ваш код
+        BufferedReader stringReader = new BufferedReader(new InputStreamReader(System.in));
+        String readText = stringReader.readLine();
+        return readText;
 
     }
 
     public static int readInt() throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      int number = scanner.nextInt();
+      return number;
 
     }
 
     public static double readDouble() throws Exception {
-        //напишите тут ваш код
+      Scanner scanner = new Scanner(System.in);
+      double number = scanner.nextDouble();
+      return number;
 
     }
 
     public static boolean readBoolean() throws Exception {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        boolean bool = scanner.nextBoolean();
+        if (bool == true) {
+        }
+      return bool;
     }
 
     public static void main(String[] args) {
