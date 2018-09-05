57c0bd5e197dff174050f3ab645410ef326dce60
xMAGWAJx <xmagwajx@gmail.com>
2018-09-05 15:27:42 +0300

    task0611 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
@@ -7,13 +7,17 @@ package com.javarush.task.task06.task0611;
 public class StringHelper {
     public static String multiply(String s) {
         String result = "";
-        //напишите тут ваш код
+        for (int i = 0; i < 5; i++){
+          result = result.concat(s);
+        }
         return result;
     }
 
     public static String multiply(String s, int count) {
         String result = "";
-        //напишите тут ваш код
+        for (int i = 0; i < count; i++){
+          result = result.concat(s);
+        }
         return result;
     }
 
