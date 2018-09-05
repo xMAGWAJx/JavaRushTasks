7906149e3fb9f6ba8ddb9598bf563170e75b580b
xMAGWAJx <xmagwajx@gmail.com>
2018-09-05 17:36:31 +0300

    task0614 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
@@ -8,16 +8,25 @@ import java.util.ArrayList;
 
 public class Cat {
     //напишите тут ваш код
+    public static ArrayList<Cat> cats = new ArrayList<Cat>();
 
     public Cat() {
+      cats.add(this);
     }
 
     public static void main(String[] args) {
         //напишите тут ваш код
+      for (int i = 0; i < 10; i++){
+        cats.add(new Cat());
+      }
         printCats();
     }
 
     public static void printCats() {
         //напишите тут ваш код
+      for (int i = 0; i < cats.size(); i++) {
+        int catsResult = cats.size() - i - 1;
+        System.out.println(cats.get(catsResult));
+      }
     }
 }
