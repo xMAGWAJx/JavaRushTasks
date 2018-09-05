68b81314d68619bb3dde13568f386e567a79c773
xMAGWAJx <xmagwajx@gmail.com>
2018-09-05 16:20:50 +0300

    task0613 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0613/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0613/Solution.java
@@ -4,16 +4,24 @@ package com.javarush.task.task06.task0613;
 Кот и статика
 */
 
+import static com.javarush.task.task06.task0613.Solution.Cat.catCount;
+
 public class Solution {
-    public static void main(String[] args) {
-        //создай 10 котов
-
-        //выведи значение переменной catCount
+  public static void main(String[] args) {
+    //создай 10 котов
+    for (int i = 0; i < 10; i++){
+      Cat cat = new Cat();
     }
+    //выведи значение переменной catCount
+    System.out.println(catCount);
+  }
 
-    public static class Cat {
-        //создай статическую переменную catCount
-
-        //создай конструктор
+  public static class Cat {
+    //создай статическую переменную catCount
+    public static int catCount = 0;
+    //создай конструктор
+    public Cat() {
+      catCount++;
     }
+  }
 }
