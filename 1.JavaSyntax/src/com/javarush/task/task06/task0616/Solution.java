45143a1207058898a09c92ee75932f371ecdce8e
xMAGWAJx <xmagwajx@gmail.com>
2018-09-06 12:45:54 +0300

    task0616 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0616/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0616/Solution.java
@@ -5,17 +5,17 @@ package com.javarush.task.task06.task0616;
 */
 
 public class Solution {
-    public int step;
+    public static int step;
 
     public static void main(String[] args) {
         method1();
     }
 
-    public void method1() {
+    public static void method1() {
         method2();
     }
 
-    public void method2() {
+    public static void method2() {
         new Solution().method3();
     }
 
