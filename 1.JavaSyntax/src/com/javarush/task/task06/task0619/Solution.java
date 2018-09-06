5e7a43ea8697097d22ad721ce75530d332df453d
xMAGWAJx <xmagwajx@gmail.com>
2018-09-06 13:39:31 +0300

    task0619 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0619/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0619/Solution.java
@@ -6,10 +6,16 @@ package com.javarush.task.task06.task0619;
 
 public class Solution {
     public static class Cat {
+      public static String name;
     }
 
     public static class Dog {
+      public static String name;
     }
+
+    public static String name;
+
+
 
     public static void main(String[] args) {
 
