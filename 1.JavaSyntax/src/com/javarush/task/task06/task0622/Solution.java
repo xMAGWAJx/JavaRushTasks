25dadd54a2620ff60f6a3adbc998a5f2e921757c
xMAGWAJx <xmagwajx@gmail.com>
2018-09-06 16:21:25 +0300

    task0622 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
@@ -9,8 +9,33 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //напишите тут ваш код
+      int[] numbers = new int[5];
+
+      for (int i = 0; i < numbers.length; i++)
+      {
+        numbers[i] = Integer.parseInt(reader.readLine());
+      }
+
+      int [] opa = bubbleSort(numbers);
+      for(int i = 0; i < opa.length; i ++)
+        System.out.println(opa[i]);
     }
+
+  public static int[] bubbleSort(int[] arr)
+  {
+    for(int i = arr.length - 1; i > 0; i--)
+    {
+      for(int j = 0; j < i; j++)
+      {
+        if(arr[j] > arr[j + 1]){
+          int t = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = t;
+        }
+      }
+    }
+    return arr;
+  }
 }
