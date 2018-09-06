c1b113a4c94b4b37a65ace6a23120ad468fe264b
xMAGWAJx <xmagwajx@gmail.com>
2018-09-06 11:25:39 +0300

    task0615 - done

--- 1.JavaSyntax/src/com/javarush/task/task06/task0615/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0615/Solution.java
@@ -6,9 +6,9 @@ package com.javarush.task.task06.task0615;
 
 public class Solution {
 
-    public int A = 5;
+    public static int A = 5;
     public int B = 2;
-    public static int C = A * B;
+    public int C = A * B;
 
     public static void main(String[] args) {
         A = 15;
