117f11e3ca06daca8eaa6d52a30fe31ad590b89a
xMAGWAJx <xmagwajx@gmail.com>
2018-09-07 14:38:23 +0300

    task0701 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0701/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0701/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task07.task0701;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+
 
 /* 
 Массивный максимум
@@ -15,13 +18,23 @@ public class Solution {
         System.out.println(max);
     }
 
-    public static int[] initializeArray() throws IOException {
-        // создай и заполни массив
-        return null;
+  public static int[] initializeArray() throws IOException {
+    // создай и заполни массив
+    int numbers[] = new int[20];
+    for(int i = 0; i < numbers.length; i++) {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      numbers[i] = Integer.parseInt(reader.readLine());
     }
+    return numbers;
+}
 
     public static int max(int[] array) {
         // найди максимальное значение
-        return 0;
+      int [] g = array;
+      int max = g[0];
+      for (int i = 1; i < array.length; i++){
+        if(g[i] > max) max = g[i];
+      }
+      return max;
     }
 }
