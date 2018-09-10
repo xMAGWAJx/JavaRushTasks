67d6c35ad4b7df3ce62a1d595982ab31aee27af7
xMAGWAJx <xmagwajx@gmail.com>
2018-09-10 16:57:31 +0300

    task0704 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
@@ -10,6 +10,16 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
+      int [] numbers = new int[10];
+
+      for (int i = 0; i < numbers.length; i++) {
+        numbers[i] = Integer.parseInt(reader.readLine());
+      }
+      for (int i = numbers.length -1; i >= 0; i--){
+        System.out.println(numbers[i]);
+      }
+
     }
 }
 
