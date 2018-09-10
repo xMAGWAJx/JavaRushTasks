f3c93ce276d6506c831d5bfdc648d3a7220fc435
xMAGWAJx <xmagwajx@gmail.com>
2018-09-10 16:43:23 +0300

    task0703 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
@@ -10,5 +10,22 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+      // Создал массив строк
+      String[] lines = new String[10];
+      // Создал массив чисел
+      int number[] = new int[10];
+
+      //заполнил массивы
+      for (int i = 0; i < lines.length; i++) {
+        lines[i] = reader.readLine();
+        number[i] = lines[i].length();
+      }
+
+      //Вывел результат
+      for (int a = 0; a < number.length; a++) {
+        System.out.println(number[a]);
+      }
     }
 }
