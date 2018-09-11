0e56eb0e63861c039f5380c71a3b809a89adc3fc
xMAGWAJx <xmagwajx@gmail.com>
2018-09-11 16:10:14 +0300

    task0705 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0705/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0705/Solution.java
@@ -10,5 +10,19 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      int numbersTwenty[] = new int[20];
+      int numbersTenFirstHalf[] = new int[10];
+      int numbersTenSecondHalf[] = new int[10];
+
+      for (int i = 0; i < numbersTwenty.length; i++){
+        numbersTwenty[i] = Integer.parseInt(reader.readLine());
+      }
+
+      for (int i = 0; i < 10; i++){
+        numbersTenFirstHalf[i] = numbersTwenty[i];
+        numbersTenSecondHalf[i] = numbersTwenty[10 + i];
+        System.out.println(numbersTenSecondHalf[i]);
+      }
     }
 }
