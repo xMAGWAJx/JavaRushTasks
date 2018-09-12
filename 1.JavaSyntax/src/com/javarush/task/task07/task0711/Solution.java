930093931456e8688ff41bb66c4106bce8ef0cd8
xMAGWAJx <xmagwajx@gmail.com>
2018-09-12 13:58:12 +0300

    task0711 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0711/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0711/Solution.java
@@ -9,7 +9,18 @@ import java.util.ArrayList;
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+  public static void main(String[] args) throws Exception {
+    //напишите тут ваш код
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    ArrayList<String> list = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      list.add(reader.readLine());
     }
+    for (int i = 0; i < 13; i++) {
+      list.add(0, list.remove(4));
+    }
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+  }
 }
