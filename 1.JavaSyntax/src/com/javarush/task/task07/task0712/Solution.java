86d194d63d2ce5a63c0d628197f7cd972c615429
xMAGWAJx <xmagwajx@gmail.com>
2018-09-12 15:11:44 +0300

    task0712 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task07.task0712;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -12,5 +11,21 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      ArrayList<String> list = new ArrayList<String>();
+      for (int i = 0; i < 10; i++) {
+        list.add(reader.readLine());
+      }
+
+      String bufferMin = (list.get(0).length() <= list.get(1).length()) ? list.get(0) : list.get(1);
+      String bufferMax = (list.get(0).length() >= list.get(1).length()) ? list.get(0) : list.get(1);
+
+      for (byte i = 2; i < 10; i++)
+      {
+        bufferMin = (bufferMin.length() <= list.get(i).length()) ? bufferMin : list.get(i);
+        bufferMax = (bufferMax.length() >= list.get(i).length()) ? bufferMax : list.get(i);
+      }
+
+      System.out.println(list.indexOf(bufferMax) > list.indexOf(bufferMin) ? bufferMin : bufferMax);
     }
 }
