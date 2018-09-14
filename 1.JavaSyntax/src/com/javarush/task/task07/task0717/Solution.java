200c3289c3e96ebd608b35181286e83a626bb0a2
xMAGWAJx <xmagwajx@gmail.com>
2018-09-14 13:39:40 +0300

    task0717 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task07.task0717;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -12,13 +13,29 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         // Считать строки с консоли и объявить ArrayList list тут
 
-        ArrayList<String> result = doubleValues(list);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      ArrayList<String> list = new ArrayList<>();
+
+      for (int i = 0; i < 10; i++) {
+        list.add(i, reader.readLine());
+      }
+
+      ArrayList<String> result = doubleValues(list);
 
         // Вывести на экран result
+      for (String x : result) {
+        System.out.println(x);
+      }
     }
 
-    public static ArrayList<String> doubleValues(ArrayList<String> list) {
-        //напишите тут ваш код
-        return null;
+    public static ArrayList<String> doubleValues(ArrayList<String> list) throws IOException {
+
+      //напишите тут ваш код
+
+      for (int i = 0; i < list.size(); i++) {
+        list.add(i + 1, list.get(i));
+        i += 1;
+      }
+        return list;
     }
 }
