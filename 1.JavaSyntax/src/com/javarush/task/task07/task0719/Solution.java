377a38e3f27f96fc323c9a789cd3f13dac17eb33
xMAGWAJx <xmagwajx@gmail.com>
2018-09-14 16:45:32 +0300

    task0719 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task07.task0719;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Вывести числа в обратном порядке
@@ -13,5 +14,16 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //напишите тут ваш код
+      ArrayList<Integer> list = new ArrayList<>();
+
+      for (int i = 0; i < 10; i++) {
+        String s = reader.readLine();
+        list.add(Integer.parseInt(s));
+      }
+
+      for (int i = 0; i < list.size(); i++) {
+        int j = list.size() - i - 1;
+        System.out.println(list.get(j));
+      }
     }
 }
