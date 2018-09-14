a41b3ddc23e5204d9a4030cce160e44c0487c22d
xMAGWAJx <xmagwajx@gmail.com>
2018-09-14 17:16:28 +0300

    task0720 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
@@ -3,15 +3,31 @@ package com.javarush.task.task07.task0720;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Перестановочка подоспела
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //напишите тут ваш код
+    //напишите тут ваш код
+
+    int N = Integer.parseInt(reader.readLine());
+    int M = Integer.parseInt(reader.readLine());
+    ArrayList<String> list = new ArrayList<>();
+
+    for (int i = 0; i < N; i++) {
+      list.add(reader.readLine());
     }
+    for (int i = 0; i < M; i++) {
+      list.add(list.get(0));
+      list.remove(0);
+    }
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+  }
 }
