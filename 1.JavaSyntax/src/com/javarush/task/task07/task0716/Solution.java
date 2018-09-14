b5a55f8f0f487dfb95e3e4df7f7b0133ef73366a
xMAGWAJx <xmagwajx@gmail.com>
2018-09-14 10:27:57 +0300

    task0716 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0716;
+
+import java.util.ArrayList;
+
+/* 
+Р или Л
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("роза"); //0
+        list.add("лоза"); //1
+        list.add("лира"); //2
+        list = fix(list);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> fix(ArrayList<String> list) {
+        //напишите тут ваш код
+        return null;
+    }
+}
