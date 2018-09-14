43a8665bcf4241825cb49d4430f461854b4c1d12
xMAGWAJx <xmagwajx@gmail.com>
2018-09-14 11:52:41 +0300

    task0716 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -7,20 +7,33 @@ import java.util.ArrayList;
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("роза"); //0
-        list.add("лоза"); //1
-        list.add("лира"); //2
-        list = fix(list);
+  public static void main(String[] args) throws Exception {
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("роза"); //0
+    list.add("лоза"); //1
+    list.add("лира"); //2
+    list.add("мера"); //2
+    list.add("вода"); //2
+    list.add("упор"); //2
+    list = fix(list);
 
-        for (String s : list) {
-            System.out.println(s);
-        }
+    for (String s : list) {
+      System.out.println(s);
     }
+  }
 
-    public static ArrayList<String> fix(ArrayList<String> list) {
-        //напишите тут ваш код
-        return null;
+  public static ArrayList<String> fix(ArrayList<String> list) {
+    //напишите тут ваш код
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).contains("р") && !list.get(i).contains("л")) {
+        list.remove(i);
+        i--;
+      } else if (list.get(i).contains("л") && !list.get(i).contains("р")) {
+        list.add(i, list.get(i));
+        i += 1;
+      } else;
     }
+    return list;
+  }
 }
+
