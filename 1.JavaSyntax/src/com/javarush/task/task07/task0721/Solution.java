aefaf329568faa344ef20eef3fd7a58def6ed0cc
xMAGWAJx <xmagwajx@gmail.com>
2018-09-18 14:47:05 +0300

    task0721 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0721/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0721/Solution.java
@@ -3,20 +3,38 @@ package com.javarush.task.task07.task0721;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Минимаксы в массивах
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int maximum;
-        int minimum;
+    int maximum;
+    int minimum;
 
-        //напишите тут ваш код
+    //напишите тут ваш код
+    int [] list = new int[20];
 
-        System.out.print(maximum + " " + minimum);
+    for (int i = 0; i < list.length; i++) {
+      list[i] = Integer.parseInt(reader.readLine());
     }
+
+    maximum = list[0];
+    minimum = list[0];
+
+    for (int i = 0; i < list.length; i++) {
+      if (list[i] > maximum) {
+        maximum = list[i];
+      }
+      if (list[i] < minimum) {
+        minimum = list[i];
+      }
+    }
+
+    System.out.print(maximum + " " + minimum);
+  }
 }
