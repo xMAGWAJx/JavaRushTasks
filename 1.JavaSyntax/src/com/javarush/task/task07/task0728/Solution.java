1a9418bb4804821b6bd733adc14c47c49e80bab7
xMAGWAJx <xmagwajx@gmail.com>
2018-09-19 17:03:22 +0300

    task0728 - done

--- 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -2,27 +2,37 @@ package com.javarush.task.task07.task0728;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /* 
 В убывающем порядке
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = new int[20];
-        for (int i = 0; i < 20; i++) {
-            array[i] = Integer.parseInt(reader.readLine());
-        }
-
-        sort(array);
-
-        for (int x : array) {
-            System.out.println(x);
-        }
+  public static void main(String[] args) throws Exception {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int[] array = new int[20];
+    for (int i = 0; i < 20; i++) {
+      array[i] = Integer.parseInt(reader.readLine());
     }
 
-    public static void sort(int[] array) {
-        //напишите тут ваш код
+    sort(array);
+
+    for (int x : array) {
+      System.out.println(x);
     }
+  }
+
+  public static void sort(int[] array) {
+    //напишите тут ваш код
+    ArrayList<Integer> list = new ArrayList<>();
+    Arrays.sort(array);
+    for (int i = 0; i < 20; i++) {
+      list.add(0, array[i]);
+    }
+    for (int i = 0; i < 20; i++) {
+      array[i] = list.get(i);
+    }
+  }
 }
