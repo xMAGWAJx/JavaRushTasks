b9cb561a5470fbf8da0cd640893c8f7e03f27dca
xMAGWAJx <xmagwajx@gmail.com>
2018-09-20 17:25:57 +0300

    task0814 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task08.task0814;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /* 
@@ -10,12 +11,17 @@ import java.util.Set;
 public class Solution {
     public static HashSet<Integer> createSet() {
         //напишите тут ваш код
-
+      HashSet<Integer> list = new HashSet<>();
+      for (int i = 0; i < 20; i++) {
+        list.add(i);
+      }
+      return list;
     }
 
     public static HashSet<Integer> removeAllNumbersMoreThan10(HashSet<Integer> set) {
         //напишите тут ваш код
-
+      set.removeIf(x -> x > 10);
+      return set;
     }
 
     public static void main(String[] args) {
