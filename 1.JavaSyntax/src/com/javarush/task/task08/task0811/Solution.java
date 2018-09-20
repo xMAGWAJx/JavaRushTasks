cf5606b36d9b2e28b2a5a8db8fbfb386cf14b508
xMAGWAJx <xmagwajx@gmail.com>
2018-09-20 16:14:27 +0300

    task0811 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
@@ -11,22 +11,26 @@ import java.util.List;
 public class Solution {
     public static List getListForGet() {
         //напишите тут ваш код
-
+      ArrayList<String> arrayGet = new ArrayList<>();
+      return arrayGet;
     }
 
     public static List getListForSet() {
         //напишите тут ваш код
-
+      ArrayList<String> arraySet = new ArrayList<>();
+      return arraySet;
     }
 
     public static List getListForAddOrInsert() {
         //напишите тут ваш код
-
+      LinkedList<String> arrayAddInset = new LinkedList<>();
+      return arrayAddInset;
     }
 
     public static List getListForRemove() {
         //напишите тут ваш код
-
+      LinkedList<String> listRemove = new LinkedList<>();
+      return listRemove;
     }
 
     public static void main(String[] args) {
