84a683a766950f4460cf4e2ffe14f44b17a53aa1
xMAGWAJx <xmagwajx@gmail.com>
2018-09-20 11:07:50 +0300

    task0801 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
@@ -5,10 +5,25 @@ HashSet из растений
 */
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+      HashSet<String> set = new HashSet<>();
+      set.add("арбуз");
+      set.add("банан");
+      set.add("вишня");
+      set.add("груша");
+      set.add("дыня");
+      set.add("ежевика");
+      set.add("женьшень");
+      set.add("земляника");
+      set.add("ирис");
+      set.add("картофель");
 
+      for (String text : set) {
+        System.out.println(text);
+      }
     }
 }
