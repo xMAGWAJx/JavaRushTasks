b7d2c7b82a29127827ee63290cc01c98362a2053
xMAGWAJx <xmagwajx@gmail.com>
2018-09-20 15:24:37 +0300

    task0808 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0808/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0808/Solution.java
@@ -9,39 +9,45 @@ import java.util.List;
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        // ArrayList
-        ArrayList arrayList = new ArrayList();
-        insert10000(arrayList);
-        get10000(arrayList);
-        set10000(arrayList);
-        remove10000(arrayList);
+  public static void main(String[] args) throws Exception {
+    // ArrayList
+    ArrayList arrayList = new ArrayList();
+    insert10000(arrayList);
+    get10000(arrayList);
+    set10000(arrayList);
+    remove10000(arrayList);
 
-        // LinkedList
-        LinkedList linkedList = new LinkedList();
-        insert10000(linkedList);
-        get10000(linkedList);
-        set10000(linkedList);
-        remove10000(linkedList);
+    // LinkedList
+    LinkedList linkedList = new LinkedList();
+    insert10000(linkedList);
+    get10000(linkedList);
+    set10000(linkedList);
+    remove10000(linkedList);
+  }
+
+  public static void insert10000(List list) {
+    //напишите тут ваш код
+    for (int i = 10000; i > 0; i--) {
+      list.add(new Object());
     }
+  }
 
-    public static void insert10000(List list) {
-        //напишите тут ваш код
-
+  public static void get10000(List list) {
+    //напишите тут ваш код
+    for (int i = 0; i < 10000; i++) {
+      list.get(i);
     }
+  }
 
-    public static void get10000(List list) {
-        //напишите тут ваш код
-
+  public static void set10000(List list) {
+    //напишите тут ваш код
+    for (int i = 0; i < 10000; i++) {
+      list.set(i, new Object());
     }
+  }
 
-    public static void set10000(List list) {
-        //напишите тут ваш код
-
-    }
-
-    public static void remove10000(List list) {
-        //напишите тут ваш код
-
-    }
+  public static void remove10000(List list) {
+    //напишите тут ваш код
+    list.clear();
+  }
 }
