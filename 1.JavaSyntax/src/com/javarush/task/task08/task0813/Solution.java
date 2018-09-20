7a0a8109a954c3ad78e83080dfb67bccbd664d5e
xMAGWAJx <xmagwajx@gmail.com>
2018-09-20 17:01:39 +0300

    task0813 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task08.task0813;
 
+import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -10,7 +11,11 @@ import java.util.HashSet;
 public class Solution {
     public static Set<String> createSet() {
         //напишите тут ваш код
-
+      HashSet<String> list = new HashSet<>();
+      for (int i = 0; i < 20; i++) {
+        list.add("Л" + i);
+      }
+      return list;
     }
 
     public static void main(String[] args) {
