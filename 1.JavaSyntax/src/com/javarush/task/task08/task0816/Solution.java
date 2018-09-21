cb32f118d8bc6ed246c8c4d8b24cd777e5842528
xMAGWAJx <xmagwajx@gmail.com>
2018-09-21 15:30:13 +0300

    task0816 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task08.task0816;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+/* 
+Добрая Зинаида и летние каникулы
+*/
+
+public class Solution {
+    public static HashMap<String, Date> createMap() throws ParseException {
+        DateFormat df = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
+        HashMap<String, Date> map = new HashMap<String, Date>();
+        map.put("Stallone", df.parse("JUNE 1 1980"));
+
+        //напишите тут ваш код
+    }
+
+    public static void removeAllSummerPeople(HashMap<String, Date> map) {
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
