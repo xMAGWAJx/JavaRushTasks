bb5cfadef7f7bfe03430acca546d4d3293d71bb0
xMAGWAJx <xmagwajx@gmail.com>
2018-09-27 15:09:37 +0300

    task0817 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task08.task0817;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /* 
@@ -10,11 +12,29 @@ import java.util.Map;
 public class Solution {
     public static HashMap<String, String> createMap() {
         //напишите тут ваш код
+        HashMap<String, String> map = new HashMap<>();
+        //map.put("Фамилия", "Имя");
+        map.put("Pepa", "Ugis");
+        map.put("Kika", "Tata");
+        map.put("Stolone", "Ugis");
+        map.put("Tuu", "Povar");
+        map.put("Peterson", "Janis");
+        map.put("Ivanov", "Janis");
+        map.put("Petrov", "Vasja");
+        map.put("Kot", "Kil");
+        map.put("QWERTY", "Ris");
+        map.put("FGHJL", "Tort");
 
+        return map;
     }
 
     public static void removeTheFirstNameDuplicates(Map<String, String> map) {
         //напишите тут ваш код
+      HashMap<String, String> copy = new HashMap<>(map);
+      for (Map.Entry<String, String> pair : copy.entrySet()) {
+        int freqeuncy = Collections.frequency(copy.values(), pair.getValue());
+        if (freqeuncy > 1) removeItemFromMapByValue(map, pair.getValue());
+      }
 
     }
 
