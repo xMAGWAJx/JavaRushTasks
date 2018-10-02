9b75576395d51685584f7a31b6d834614ac4a876
xMAGWAJx <xmagwajx@gmail.com>
2018-10-02 14:03:33 +0300

    task0818 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0821/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0821/Solution.java
@@ -15,8 +15,20 @@ public class Solution {
 
     public static Map<String, String> createPeopleList() {
         //напишите тут ваш код
+      HashMap<String, String> map = new HashMap<>();
 
-        return null;
+      map.put("Pepa", "Ugis");
+      map.put("Pepa", "Ugis");
+      map.put("Stolone", "Ugis");
+      map.put("Tuu", "Povar");
+      map.put("Peterson", "Janis");
+      map.put("Ivanov", "Janis");
+      map.put("Petrov", "Vasja");
+      map.put("Kot", "Kil");
+      map.put("QWERTY", "Ris");
+      map.put("FGHJL", "Tort");
+
+      return map;
     }
 
     public static void printPeopleList(Map<String, String> map) {
