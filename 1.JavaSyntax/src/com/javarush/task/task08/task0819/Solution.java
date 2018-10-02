3596217d446adcd02c5b2f6da6704ff730d65dad
xMAGWAJx <xmagwajx@gmail.com>
2018-10-02 13:25:53 +0300

    task0818 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
@@ -8,22 +8,38 @@ Set из котов
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        Set<Cat> cats = createCats();
+  public static void main(String[] args) {
+    Set<Cat> cats = createCats();
 
-        //напишите тут ваш код. step 3 - пункт 3
+    //напишите тут ваш код. step 3 - пункт 3
+    cats.remove(cats.toArray()[0]);
+    printCats(cats);
+  }
 
-        printCats(cats);
+  public static Set<Cat> createCats() {
+    //напишите тут ваш код. step 2 - пункт 2
+    HashSet<Cat> catSet = new HashSet<Cat>();
+    Cat newCat = new Cat();
+    catSet.add(newCat);
+
+    newCat = new Cat();
+    catSet.add(newCat);
+
+    newCat = new Cat();
+    catSet.add(newCat);
+
+    return catSet;
+  }
+
+  public static void printCats(Set<Cat> cats) {
+    // step 4 - пункт 4
+    for (Cat cat : cats) {
+      System.out.println(cat);
     }
+  }
 
-    public static Set<Cat> createCats() {
-        //напишите тут ваш код. step 2 - пункт 2
-        return null;
-    }
+  // step 1 - пункт 1
+  public static class Cat {
 
-    public static void printCats(Set<Cat> cats) {
-        // step 4 - пункт 4
-    }
-
-    // step 1 - пункт 1
+  }
 }
