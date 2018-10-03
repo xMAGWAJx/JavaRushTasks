1f2fc022bca2a495f52c18036fd549997eadd53f
xMAGWAJx <xmagwajx@gmail.com>
2018-10-03 15:41:45 +0300

    task0829 - done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /* 
@@ -15,20 +16,15 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //list of addresses
-        List<String> addresses = new ArrayList<String>();
-        while (true) {
-            String family = reader.readLine();
-            if (family.isEmpty()) break;
-
-            addresses.add(family);
-        }
-
-        //read home number
-        int houseNumber = Integer.parseInt(reader.readLine());
-
-        if (0 <= houseNumber && houseNumber < addresses.size()) {
-            String familySecondName = addresses.get(houseNumber);
-            System.out.println(familySecondName);
-        }
+      HashMap<String, String> addresses = new HashMap<>();
+      while (true) {
+        String city = reader.readLine();
+        if (city.isEmpty())
+          break;
+        String family = reader.readLine();
+        addresses.put(city, family);
+      }
+      String findCity = reader.readLine();
+      System.out.println(addresses.get(findCity));
     }
 }
